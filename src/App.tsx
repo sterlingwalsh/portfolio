adf5cac403fbfba8ffc6d637c598d39cc180b5a0
sterling.walsh <sterling.walsh@seisan.com>
2022-03-14 21:20:30 -0400

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { TreeMaker } from './components/tree/widgetpicker';
 import './styles/index.scss';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { ConfirmProvider } from './components/confirmation/confirmprovider';
 import { Home } from './pages/home.page';
 
-type TreeItem = {
-    title: string;
-    children: TreeItem[];
-};
-const options: TreeItem[] = new Array(10).fill(null).map((n, i) => ({
-    title: i + '',
-    children: [],
-}));
-
 function App() {
     return (
         <ConfirmProvider>
-            <BrowserRouter>
+            <BrowserRouter basename='/portfolio2'>
                 <Routes>
                     <Route path='/'>
                         <Route index element={<Home />} />
@@ -35,18 +24,6 @@ function App() {
                 </Routes>
             </BrowserRouter>
         </ConfirmProvider>
-        // <div className='App'>
-        //     <TreeMaker
-        //         options={options}
-        //         getItemlabel={(o) => o.title}
-        //         getChildren={(o) => o.children}
-        //         setChildren={(o, c) => ({
-        //             ...o,
-        //             children: c,
-        //         })}
-        //         optionIsContainer={(o) => !(parseInt(o.title) % 3)}
-        //     />
-        // </div>
     );
 }
 
